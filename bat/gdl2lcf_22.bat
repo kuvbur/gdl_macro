ed3de8d579339858191c6e49649b56c12f6c1f1b
kuvbur <kuvbur@gmail.com>
2020-05-11 14:39:52 +0300

    add reinforce

--- bat/gdl2lcf_22.bat
+++ bat/gdl2lcf_22.bat
@@ -1,10 +1,11 @@
 cd /d %~dp0
 cd ..
-set "gdl_dir=%cd%\gdl"
 
+set "gdl_dir=%cd%\gdl"
 set "gdl_glob_dir=%cd%\Libraryflobals\gdl"
+set "bat_dir=%cd%\bat"
 
 cd lcf
-"C:\Program Files\GRAPHISOFT\ARCHICAD 22\LP_XMLConverter.exe"  createcontainer  macro.lcf "%gdl_dir%">%bat_dir%\lcf_log.txt
-"C:\Program Files\GRAPHISOFT\ARCHICAD 22\LP_XMLConverter.exe"  createcontainer  Libraryflobals.lcf "%gdl_glob_dir%">%bat_dir%\lcf_log.txt
-"C:\Program Files\GRAPHISOFT\ARCHICAD 22\LP_XMLConverter.exe"  createcontainer  Libraryflobals_and_macro.lcf "%gdl_glob_dir%" "%gdl_dir%">%bat_dir%\lcf_log.txt
+"C:\Program Files\GRAPHISOFT\ARCHICAD 22\LP_XMLConverter.exe"  createcontainer  macro.lcf -compress 9 "%gdl_dir%">"%bat_dir%\_lcf_log.txt"
+"C:\Program Files\GRAPHISOFT\ARCHICAD 22\LP_XMLConverter.exe"  createcontainer  Libraryflobals.lcf -compress 9 "%gdl_glob_dir%">"%bat_dir%\_lcf_log.txt"
+"C:\Program Files\GRAPHISOFT\ARCHICAD 22\LP_XMLConverter.exe"  createcontainer  Libraryflobals_and_macro.lcf -compress 9 "%gdl_glob_dir%" "%gdl_dir%">"%bat_dir%\_lcf_log.txt"
