8cfda7ecc92ee651be3ad93c60a1aa615fb4e7a5
kuvbur <kuvbur@gmail.com>
2021-11-10 16:00:18 +0300

    AC25

---
+++ bat/gdl2xml_25.bat
@@ -0,0 +1,16 @@
+cd /d %~dp0
+cd ..
+set "gdl_dir=%cd%\gdl"
+set "xml_dir=%cd%\xml"
+set "hsf_dir=%cd%\hsf"
+set "bat_dir=%cd%\bat"
+
+"C:\Program Files\GRAPHISOFT\ARCHICAD 25\LP_XMLConverter.exe"  l2x -l UTF8 "%gdl_dir%" "%xml_dir%" >"%bat_dir%\_xml_log.txt"
+"C:\Program Files\GRAPHISOFT\ARCHICAD 25\LP_XMLConverter.exe"  l2hsf "%gdl_dir%" "%hsf_dir%" >"%bat_dir%\_hsf_log.txt"
+
+set "gdl_glob_dir=%cd%\Libraryflobals\gdl"
+set "xml_glob_dir=%cd%\Libraryflobals\xml"
+set "hsf_glob_dir=%cd%\Libraryflobals\hsf"
+
+"C:\Program Files\GRAPHISOFT\ARCHICAD 25\LP_XMLConverter.exe"  l2x -l UTF8 "%gdl_glob_dir%" "%xml_glob_dir%" >"%bat_dir%\_xml_glob_log.txt"
+"C:\Program Files\GRAPHISOFT\ARCHICAD 25\LP_XMLConverter.exe"  l2hsf "%hsf_glob_dir%" "%xml_glob_dir%" >"%bat_dir%\_hsf_glob_log.txt"
